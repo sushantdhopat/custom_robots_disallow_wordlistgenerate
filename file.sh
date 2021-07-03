3f54cb10bda638f9f359ecfb57454467e807ee69
sushant_dhopat <57283755+sushantdhopat@users.noreply.github.com>
2021-07-03 14:03:59 +0530

    Create file.sh

---
+++ file.sh
@@ -0,0 +1,13 @@
+echo -e "\e[1;32m "
+target=$1
+
+echo -e "\e[1;34m [+] finding robots.txt file from subdomian \e[0m"
+httpx -l $1 -paths /robots.txt -silent -o robots-url.txt
+
+echo -e "\e[1;34m [+] finding disallow paths from the robots.txt \e[0m"
+for url in $(cat robots-url.txt);do http -b $url | grep 'Disallow' | awk -F ' ' '{print $2}' | cut -c 2- | anew robot-words.txt;done
+
+echo -e "\e[1;34m [+] pasing this file to fuff for the fuzzing \e[0m"
+ffuf -w validsubdomain-iagcargo.com.txt:URL -w robot-words.txt:DIR -u URL/DIR -mc 200,403 -t 350 -of html -o result.html
+
+echo -e "\e[1;34m [+] Finished all recon \e[0m"

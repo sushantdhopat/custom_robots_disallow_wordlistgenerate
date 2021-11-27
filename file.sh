876c47010dccfdc892b3b17c7260a0c8559df4b1
sushant_dhopat <57283755+sushantdhopat@users.noreply.github.com>
2021-11-27 13:56:32 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -7,7 +7,6 @@ httpx -l $1 -paths /robots.txt -silent -o robots-url.txt
 echo -e "\e[1;34m [+] finding disallow paths from the robots.txt \e[0m"
 for url in $(cat robots-url.txt);do http -b $url | grep 'Disallow' | awk -F ' ' '{print $2}' | cut -c 2- | anew robot-words.txt;done
 
-echo -e "\e[1;34m [+] pasing this file to fuff for the fuzzing \e[0m"
-ffuf -w $1:URL -w robot-words.txt:DIR -u URL/DIR -mc 200,403 -t 350 -of html -o result.html
-
+echo -e "\e[1;34m [+] pasing this file to dirsearch  for the fuzzing \e[0m"
+sudo dirsearch -l /home/sushant/new-sega.com/validsubdomain-sega.com.txt -w /home/sushant/new-sega.com/robot-words.txt --extensions=EXTENSIONS -o newresult
 echo -e "\e[1;34m [+] Finished all recon \e[0m"

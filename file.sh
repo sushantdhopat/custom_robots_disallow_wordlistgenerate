21ed0f94e081d8b3385f4b0f8334543a85575c5d
sushant dhopat <57283755+sushantdhopat@users.noreply.github.com>
2023-03-16 21:04:17 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,6 +1,4 @@
 echo -e "\e[1;32m "
-target=$1
-target1=$2
 
 echo -e "\e[1;34m [+] finding robots.txt file from subdomain \e[0m"
 httpx -l $1 -paths /robots.txt -silent -o robots-url.txt
@@ -8,6 +6,4 @@ httpx -l $1 -paths /robots.txt -silent -o robots-url.txt
 echo -e "\e[1;34m [+] finding disallow paths from the robots.txt \e[0m"
 for url in $(cat robots-url.txt);do http -b $url | grep 'Disallow' | awk -F ' ' '{print $2}' | cut -c 2- | anew robot-words.txt;done
 
-echo -e "\e[1;34m [+] pasing this file to dirsearch  for the fuzzing \e[0m"
-sudo dirsearch -l $target1 -w /home/sushant/robot-words.txt --extensions=EXTENSIONS -o newresult
 echo -e "\e[1;34m [+] Finished all recon \e[0m"
